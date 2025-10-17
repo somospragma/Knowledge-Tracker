76e5365e04e4a45da501eb9054b4d416d931e103
Ricardo Paredes <ricardo.paredes@pragma.com.co>
2025-10-16 23:56:05 -0500

    Spring Project Setup

---
+++ src/main/java/com/pragma/vc/tracker/KnowledgeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.vc.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/vc/tracker/KnowledgeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.vc.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
